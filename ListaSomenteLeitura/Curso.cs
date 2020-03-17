4283de4e3abfa0335bfe40f95b0e69ba356ec874
Caio theodoro <caio.theodoro@hotmail.com>
2020-03-17 16:42:39 -0300

    Utilizando a classe genérica ReadOnly<T> e encapsulando dependencias de Curso

---
+++ ListaSomenteLeitura/Curso.cs
@@ -0,0 +1,47 @@
+﻿using LearningThroughCollections;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ListaSomenteLeitura
+{
+    class Curso
+    {
+		private IList<Aula> aulas;
+		private string instrutor;
+		private string nome;
+
+		public Curso(string instrutor, string nome)
+		{
+			this.instrutor = instrutor;
+			this.nome = nome;
+			this.aulas = new List<Aula>();
+		}
+
+		internal void Adiciona(Aula aula)
+		{
+			this.aulas.Add(aula);
+		}
+
+		public IList<Aula> Aulas
+		{
+			get { return new ReadOnlyCollection<Aula>(aulas); }
+			
+		}
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		public string Instrutor 
+		{
+			get { return instrutor; }
+			set { instrutor = value; }
+		}
+
+
+	}
+}
---
+++ ListaSomenteLeitura/Program.cs
@@ -0,0 +1,57 @@
+﻿using ListaSomenteLeitura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningThroughCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharColecoes = new Curso("C# Collections", "Marcelo Silva");
+            csharColecoes.Adiciona(new Aula("Trabalhando com listas", 21));
+            Imprimir(csharColecoes.Aulas);
+        }
+
+        private static void Imprimir(IList<Aula> list)
+        {
+            Console.Clear();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Aula : IComparable
+    {
+        //Aula herda de object
+        //O toString é o de Object namespace.NomeDaClasse
+
+        private string titulo;
+        private int tempo;
+
+        public Aula(string titulo, int tempo)
+        {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+
+        public int Tempo { get => this.tempo; set => this.tempo = value; }
+        public String Titulo { get => this.titulo; set => this.titulo = value; }
+
+        public int CompareTo(object obj)
+        {
+            Aula that = obj as Aula;
+            return this.titulo.CompareTo(that.titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"[título {this.titulo}, tempo: {this.tempo} minutos]";
+        }
+    }
+}
